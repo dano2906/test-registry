c0730156cd1aea6410b63d846c225ab63964b8df
dano2906 <dlg9806@gmail.com>
2026-01-04 12:56:20 -0500

    create first registry item

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Button from "@/components/ui/button/Button.vue";
+import CustomButton from "@/registry/new-york/custom-button/CustomButton.vue";
 </script>
 
 <template>
   <div>
     <Button>Mi boton basico</Button>
+    <CustomButton />
   </div>
 </template>
---
+++ src/registry/new-york/custom-button/CustomButton.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { Eye } from "lucide-vue-next";
+</script>
+
+<template>
+  <Button>CustomButton <Eye /></Button>
+</template>
